f183f78fea451851122dee74529b76eacba9bbeb
vinitha <selvivini@gmail.com>
2021-02-23 15:29:05 -0600

    added api routes

--- server.js
+++ server.js
@@ -1,12 +1,43 @@
 const express = require('express');
 const path = require('path');
+const fs= require('fs');
+const noteData = require('./db/db.json')
 const app= express();
 const PORT =process.env.PORT || 3000
 
+app.use(express.static('public'))
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
 
-app.get('*', (req,res)=>{
-    res.sendFile(path.join(__dirname, '/public/assets/index.html'))
+
+app.get('/api/notes',(req,res)=>{
+    
+    res.send(noteData)
 })
+
+app.post('/api/notes',(req,res)=>{
+ let dataPath = path.join(__dirname, "db/db.json")
+ let newNote = req.body;
+ console.log(newNote)
+noteData.push(newNote)
+ fs.writeFile(dataPath, JSON.stringify(noteData), (err)=>{
+ if(err){
+     console.log(err)
+ }else{
+     console.log("your note is saved")
+ }
+ })
+ res.json(newNote)
+})
+
+app.get('/notes',(req,res)=>{
+    res.sendFile(path.join(__dirname,'public/notes.html'))
+    
+})
+app.get('/', (req,res)=>{
+    res.sendFile(path.join(__dirname, 'public/index.html'))
+})
+
 
 
 app.listen(PORT, ()=>{
